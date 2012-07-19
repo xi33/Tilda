1ee2f927bf63a4adc4d33f7ba653ad1354afb944
Pieux Xi <pieuxxi@gmail.com>
2012-07-19 17:44:30 +0800

    design stuff entity

---
+++ src/Model/Stuff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Stuff
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Team { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
